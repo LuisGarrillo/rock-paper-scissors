dae9d10c9c0df3d9143e5975ce69ccefadbc77af
Luis Garrillo <garrillofeeling@gmail.com>
2024-05-04 17:01:35 -0400

    Add computer choice function

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
+let validChoices = ["rock", "paper", "scissors"];
+
 function getHumanChoice() {
-    let validChoices = ["rock", "paper", "scissors"];
+    
     let valid = false;
+    let choice = "";
 
     while(!valid) {
-        let choice = prompt("Select a weapon from rock, paper or scissors!");
+        choice = prompt("Select a weapon from rock, paper or scissors!");
         choice = choice.trim().toLowerCase();
 
         valid = Boolean(validChoices.find((option) => option === choice));
     }
-    
+    return choice; 
+}
+
+function getComputerChoice() {
+    index = Math.floor(Math.random() * 3);
+    return validChoices[index];
 }
 
 let humanScore = 0, computerScore = 0;
 
 for (let i = 1; i < 6; i++) {
-    getHumanChoice()
-    //get computer choice
+    console.log(`Round #${i}`);
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
 
+    console.log(`Human: ${humanChoice} VS Computer: ${computerChoice}`);
     //compare choices
     //add score
 }
